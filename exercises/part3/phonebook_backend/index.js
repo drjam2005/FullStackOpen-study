9f5edab0eadbcb8973e5a46b72761a64c59d9841
drjam2005 <jamesallanmdr2005@gmail.com>
2026-07-07 19:19:08 +0800

    chore: finished exercises 3.7 -> 3.8

--- exercises/part3/phonebook_backend/index.js
+++ exercises/part3/phonebook_backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method);
+	console.log('Path:  ', request.path);
+	console.log('body:  ', request.body);
+	console.log('---');
+	next();
+}
+
+const unknownEndpoint = (request, response, next) => {
+	response.status(404).send({error: 'unknown endpoint'});
+	next();
+}
+
 app.use(express.json())
+// app.use(requestLogger);
+morgan.token('body', (req) => { return JSON.stringify(req.body)});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let phonebook = [
     {
@@ -97,6 +114,8 @@ app.get('/info', (request, response) => {
 		</div>
 	`);
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
